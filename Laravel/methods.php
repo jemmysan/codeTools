e69a2b84347f05a49c22e232a33ce44ce7eba144
JEMMY <jemmy1399@gmail.com>
2023-11-17 23:19:52 +0000

    Tools

--- Laravel/methods.php
+++ Laravel/methods.php
@@ -24,3 +24,31 @@ class ResponseMessage extends Controller
         return response()->json($response);
     }
 }
+
+
+
+/********************** Supprimer un enregistrement avec la table d'association ******/
+
+class deleteAssociation
+{
+    public function delete($id)
+    {
+        return DB::transaction(function () use($id){
+            $model = Model::find($idArticle);
+            if($model)
+            {
+                // $model->associationMethod()->update(['deleted_at' => now()]);
+                $association = Association::where('id',$idArticle)->get();
+                foreach ($association as $items) {
+                    $items->update(['deleted_at' => now()]);
+                }
+                $model->delete();
+                return response()->json(['messages'=>'article supprimé avec succès']);
+            }
+            else
+            {
+                return response()->json(['messages'=>'article introuvable']);
+            }
+        });
+    }
+}
--- Laravel/query.php
+++ Laravel/query.php
@@ -6,3 +6,8 @@ return Model::where('first_name','like','%'.$keyWord.'%');
 
 /********** Inserer plusiurs fois dans une table d'association *******/
 $table->methode()->attach($request->tabRequest);
+
+
+/********** Voir les elements supprimée avec softDelte *******/
+$Model::onlyTrashed()->latest()->get();
+
